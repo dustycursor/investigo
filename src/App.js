05759a12e1dec40b29dca0679fc7f98d7f3a2068
Kenneth Isom <kennethisom@gmail.com>
2019-04-14 21:02:27 -0500

    Initial prototyping.  Input recognition and parsing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Papa from 'papaparse';
+
 import './App.css';
 
+const inputIsFromBankOfAmerica = input => {
+  input = input.split('\n');
+
+  if (input.length > 8 && input[0].trim() === 'Description,,Summary Amt.' && input[6].trim() === 'Date,Description,Amount,Running Bal.') {
+    let header = Papa.parse(input.splice(0, 6).join('\n'), {header: true});
+    console.log(header);
+    return header.data[0]['Description'].startsWith('Beginning balance as of')
+      && header.data[1]['Description'] === 'Total credits'
+      && header.data[2]['Description'] === 'Total debits'
+      && header.data[3]['Description'].startsWith('Ending balance as of');
+  }
+
+  return false;
+}
+
+const parseBankOfAmericaInput = input => {
+  input = input.split('\n').splice(6).join('\n');
+  return Papa.parse(input, {header: true});
+}
+
+const inputIsFromCiti = input => {
+  input = input.split('\n');
+  return input.length > 1 && input[0].trim() === 'Status,Date,Description,Debit,Credit,Member Name';
+}
+
+const parseCitiInput = input => {
+  return Papa.parse(input, {header: true});
+}
+
 class App extends Component {
+  state = {data: ''};
+
+  onChange = event => {
+    let input = event.target.value;
+    if (inputIsFromBankOfAmerica(input)) {
+      console.log('Found Bank of America Input');
+      console.log(parseBankOfAmericaInput(input));
+    }
+    if (inputIsFromCiti(input)) {
+      console.log('Found Citi Input');
+      console.log(parseCitiInput(input));
+    }
+    this.setState({data: input});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <textarea cols="150" rows="30" onChange={this.onChange}></textarea>
+        </div>
+        <hr />
+        <pre>
+          {this.state.data}
+        </pre>
       </div>
     );
   }
